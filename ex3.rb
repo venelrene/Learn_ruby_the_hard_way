a172484102175ab19e8144920850562a52465714
venelrene <venelrene@gmail.com>
2019-08-04 00:34:13 -0400

    first commit

---
+++ ex3.rb
@@ -0,0 +1,32 @@
+# Explaining what I'm going to do
+puts "I will now count my chickens:"
+# count the hens by inserting computation into the text
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# count the rooster by inserting computation into the text
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+#  Enplaning what  I'm going to do next
+puts "Now I will count the eggs:"
+# Math problem
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+# printing next question
+puts "Is it true that 3.0 + 2.0 < 5 .0- 7.0?"
+# printing the math problem
+puts "3.0 + 2.0 < 5.0 - 7.0"
+
+# solving and printing the math problem through insertion
+puts "What is 3.0 + 2.5? #{3.0 + 2.5}"
+# same as about line :17
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+# reason why its going to return false
+puts "Oh, that's why it's false."
+# printing out more question
+puts "How about some more."
+# solving and printing the math problem through insertion
+puts "Is it greater? #{5.5 > -2.0}"
+# same as line :24
+puts "Is it greater or equal? #{5.5 >= -2.5}"
+# same as line :24
+puts "Is it less or equal? #{5.5 <= -2.5}"
+
+
+
---
+++ sd_ex3.rb
@@ -0,0 +1,5 @@
+puts "This is a simple math problem"
+
+puts "what is 3 * 9 / 6 + 10 % 4?"
+
+puts "Answer: #{3 * 9 / 6 + 10 % 4 }"
